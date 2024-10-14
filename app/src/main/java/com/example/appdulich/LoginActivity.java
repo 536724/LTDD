2f396121af3d6f5cfe0ffd8ea89b670eeaccf187
536724 <huenguyen120304@gmail.com>
2024-10-14 11:45:14 +0700

    commit_14_10_Hue

--- app/src/main/java/com/example/appdulich/LoginActivity.java
+++ app/src/main/java/com/example/appdulich/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.appdulich;
 import android.content.Intent;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends Activity {
@@ -16,5 +18,17 @@ public class LoginActivity extends Activity {
             Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
             startActivity(intent);
         });
+        Button btnLogin = findViewById(R.id.loginButton);
+
+        // Sự kiện nhấn nút "Đăng nhập"
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Chuyển sang NotificationActivity
+                Intent intent = new Intent(LoginActivity.this, NotificationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
