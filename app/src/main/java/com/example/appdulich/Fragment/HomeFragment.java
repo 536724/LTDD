b021a20e469c4c07a489084f450320f0600716e3
536724 <huenguyen120304@gmail.com>
2024-10-14 23:40:59 +0700

    hue_commit_master

--- app/src/main/java/com/example/appdulich/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/appdulich/Fragment/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.appdulich.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,11 +11,16 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.appdulich.Adapter.CityAdapter;
 import com.example.appdulich.Adapter.TicketAdapter;
+import com.example.appdulich.CartActivity;
+import com.example.appdulich.LoginActivity;
 import com.example.appdulich.Model.City;
 import com.example.appdulich.Model.Ticket;
+import com.example.appdulich.NotificationActivity;
 import com.example.appdulich.R;
 
 import java.util.ArrayList;
@@ -25,8 +31,7 @@ public class HomeFragment extends Fragment {
     RecyclerView rvTicket;
     RecyclerView rvCity;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_home, container, false);
         rvCity = v.findViewById(R.id.rcv_city);
@@ -41,13 +46,33 @@ public class HomeFragment extends Fragment {
         rvTicket.setAdapter(ticketAdapter);
         rvCity.setLayoutManager(linearLayoutManager);
         rvCity.setAdapter(cityAdapter);
+
+        ImageButton iconGioHang = v.findViewById(R.id.icon_giohang);
+        ImageButton iconThongBao = v.findViewById(R.id.imageButton);
+
+        iconGioHang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), CartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        iconThongBao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), NotificationActivity.class);
+                startActivity(intent);
+            }
+        });
         return v;
+
     }
 
     public void initDataRvCity(){
         list = new ArrayList<>();
         City city1 = new City(R.drawable.notifications, "Ha Noi"); list.add(city1);
-        City city2 = new City(R.drawable.notifications, "Hoo Chi Minh"); list.add(city2);
+        City city2 = new City(R.drawable.notifications, "Ho Chi Minh"); list.add(city2);
         City city3 = new City(R.drawable.notifications, "Da Nang"); list.add(city3);
         City city4 = new City(R.drawable.notifications, "Da Lat"); list.add(city4);
     }
@@ -57,8 +82,9 @@ public class HomeFragment extends Fragment {
         Ticket ticket1 = new Ticket(R.drawable.img_chitiet_xebuyt, "Vé cáp treo Bà Nà\n Hill_ Đà ...",4.7, 10000, 800);ticketArrayList.add(ticket1);
         Ticket ticket2 = new Ticket(R.drawable.img_chitiet_xebuyt, "Vé Dà Nẵng\nDowntown.....",4.7, 870, 564);ticketArrayList.add(ticket2);
         Ticket ticket3 = new Ticket(R.drawable.img_chitiet_xebuyt, "Vé xe lửa Hà\nNội - Lào Cai",4.7, 126, 309);ticketArrayList.add(ticket3);
-        Ticket ticket4 = new Ticket(R.drawable.img_chitiet_xebuyt, "Vé À Ố Show Ở\nNhà Hát Thàn...",4.7, 368, 599);ticketArrayList.add(ticket4);
+        Ticket ticket4 = new Ticket(R.drawable.aoshow, "Vé À Ố Show Ở\nNhà Hát Thàn...",4.7, 368, 599);ticketArrayList.add(ticket4);
         Ticket ticket5 = new Ticket(R.drawable.img_chitiet_xebuyt, "Vé cáp treo Bà Nà\n Hill_ Đà ...",4.7, 1287, 760);ticketArrayList.add(ticket5);
         Ticket ticket6 = new Ticket(R.drawable.img_chitiet_xebuyt, "Vé cáp treo Bà Nà\n Hill_ Đà ...",4.7, 1923, 900);ticketArrayList.add(ticket6);
     }
+
 }
--- app/src/main/java/com/example/appdulich/NotificationActivity.java
+++ app/src/main/java/com/example/appdulich/NotificationActivity.java
@@ -4,7 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NotificationActivity extends AppCompatActivity {
@@ -12,17 +13,14 @@ public class NotificationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_thongbao);
+        setContentView(R.layout.activity_notification);
 
-        Button backIcon = findViewById(R.id.orderUpdateButton);
-
-        // Sự kiện nhấn icon quay lại
-        backIcon.setOnClickListener(new View.OnClickListener() {
+        ImageButton quayLai = findViewById(R.id.backButton);
+        quayLai.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Chuyển sang CartActivity (Giỏ hàng)
-                Intent intent = new Intent(NotificationActivity.this, CartActivity.class);
-                startActivity(intent);
+
+                finish();
             }
         });
     }
